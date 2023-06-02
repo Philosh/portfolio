70dfac6482278e29e42c7225794682261b5157d6
SaugatPersonal <SaugatPersonal@Saugats-MacBook-Pro.local>
2023-06-02 20:41:40 +0300

    Basic addition of tasks html

--- routes/interviewTasks.js
+++ routes/interviewTasks.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 /* GET users listing. */
+
 router.get("/", function (req, res, next) {
-  res.send("Displaydd datavis here");
+  res.sendFile(path.resolve("views/tasksViews/tasksIndex.html"));
 });
 
 module.exports = router;
