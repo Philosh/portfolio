51d77496ce2e4a982df137bdcce77069aaa8afcb
SaugatPersonal <SaugatPersonal@Saugats-MacBook-Pro.local>
2023-06-04 16:04:05 +0300

    Front end coming to file

--- routes/interviewTasks.js
+++ routes/interviewTasks.js
@@ -6,10 +6,21 @@ const TASKS = require("../taskSolutions/taskMain.js");
 /* GET users listing. */
 
 router.get("/", function (req, res, next) {
-  res.sendFile(path.resolve("views/tasksViews/tasksIndex.html"));
+  res.render("tasksViews/tasksMain");
 });
 
-router.get("/1", function (req, res, next) {
+router.get("/:taskId", function (req, res, next) {
+  console.log("params", req.params.taskId);
+  const taskID = req.params.taskId;
+  const taskIdFunc = {
+    1: TASKS.task1,
+    2: TASKS.task2,
+    3: TASKS.task3,
+    4: TASKS.task4,
+    5: TASKS.task5,
+    6: TASKS.task6,
+    7: TASKS.task7,
+  };
   //const answer1 = TASKS.task1(2, "100");
   //console.log("answer1", answer1);
 
@@ -22,9 +33,12 @@ router.get("/1", function (req, res, next) {
   // const answer4 = TASKS.task4(100);
   // console.log(("answer4", answer4));
 
-  const answer5 = TASKS.task5(99);
-  console.log("answer5", answer5);
-  res.render("tasksViews/tasksDev", { ans: answer5 });
+  const taskFunc = taskIdFunc[taskID];
+  const answer = taskFunc(2, "100");
+  console.log("answer", answer);
+  const answer1 = TASKS.task1(2, "100");
+  console.log("answer5", answer1);
+  res.render("tasksViews/tasksMain", { ans: answer });
 });
 
 module.exports = router;
