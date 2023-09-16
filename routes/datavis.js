dd38115374215e049e75b4f5f0aaa3f2a8147491
SaugatPersonal <SaugatPersonal@Saugats-MacBook-Pro.local>
2023-09-16 21:17:22 +0300

    Added MyResume

--- routes/datavis.js
+++ routes/datavis.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("Displaydd datavis here");
+  res.send("Displayed datavis here");
 });
 
 module.exports = router;
